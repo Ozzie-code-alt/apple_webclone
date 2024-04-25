cf84b9aebf13841bfb83985ea983c28c896166cf
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2024-04-25 19:34:01 +0800

    Finished : Features - How it Works and footer Page - Sentry

--- apple-clone/src/App.jsx
+++ apple-clone/src/App.jsx
@@ -2,6 +2,9 @@ import NavbarApple from "./components/NavbarApple";
 import HeroApple from "./components/HeroApple";
 import HighlighApple from "./components/HighlighApple";
 import Model from "./components/Model";
+import Features from "./components/Features";
+import HowitWorks from "./components/HotitWorks";
+import Footer from "./components/Footer";
 const App = () => {
   return (
     <main className="">
@@ -9,6 +12,9 @@ const App = () => {
       <HeroApple />
       <HighlighApple />
       <Model/>
+      <Features/>
+      <HowitWorks/>
+      <Footer/>
     </main>
   );
 };
